d5ec5897381783d4562c5a9d708f0e197d6191a0
twskj <Tawesak.Jongsuksawas@chrobinson.com>
2018-12-25 11:12:54 -0600

    feat: new guess engine + error feedback

--- main.js
+++ main.js
@@ -2,49 +2,333 @@ function notify(err) {
     console.log(err);
 }
 
-function guess(node, selection) {
-    if (!node) {
-        return;
-    }
-    var text = node.innerText || node.value;
-    var start_idx = text.indexOf("{");
-    var end_idx = text.lastIndexOf("}");
-    if (start_idx == -1 || end_idx == -1 || end_idx < start_idx) {
-        return "";
-    }
-    return text.substr(start_idx, end_idx - start_idx + 1);
-
-    //TODO: use stack(s) to figure out nearest object
-    // find longest sequence inside-out, outside-in
-    // 1. click outside of json (left of json)
-    // 2. click outside of json (right of json)
-    // 3. click in json --> working outward --> using stack
-    // 4. click between json -> prefer closer json
+function getInitialState() {
+    return {
+        tokens: []
+        , context: []
+        , inQuote: false
+        , jsonIdx: -1
+        , quoteIdx: -1
+    };
 }
 
-function wrapWithPreCode(node, prefix, text, suffix) {
-    var pre = document.createElement("pre");
-    var code = document.createElement("code");
+function popUntil(stack, val) {
+    while (stack.length > 0) {
+        if (val === stack.pop()) {
+            return;
+        }
+    }
+}
 
-    pre.appendChild(code);
-    code.innerText = text;
+function addCSS() {
+    if (!document.getElementById("JipyCSS")) {
+        var style = document.createElement('style');
+        style.id = "JipyCSS";
+        style.type = 'text/css';
+        style.innerHTML =
+            `@keyframes Jippy-error-flash {
+    0% {
+        background-color: red;
+        opacity: 1;
+    }
+    100% {
+        background-color: inherit;
+    }
+}
+.jippy-cant-parse {
+    animation-name: Jippy-error-flash;
+    animation-duration: 500ms;
+    animation-iteration-count: 1;
+    animation-timing-function: ease;
+}`;
+        document.head.appendChild(style);
+    }
+}
 
-    node.innerText = "";
+function extractJSONs(str) {
+    var jsons = [];
+    var numbers = new Set(['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'])
+    var state = getInitialState();
+
+    for (var i = 0; i < str.length; i++) {
+        ch = str[i];
+
+        if (state.inQuote) {
+            if (ch === '"') {
+                state.inQuote = false;
+                state.tokens.push('v');
+            }
+            else if (ch === '\\') {
+                i++;
+            }
+            continue;
+        }
+        if (ch === "\n" || ch === " " || ch === "\t" || ch == "\r") {
+            continue;
+        }
+        if (state.context.length === 0 && (ch === '{' || ch === '[')) {
+            state.tokens.push(ch);
+            state.context.push(ch === "{" ? 'o' : 'a');
+            if (state.jsonIdx === -1) {
+                state.jsonIdx = i;
+            }
+            continue;
+        }
+        if (!state.context.length) {
+            continue;
+        }
+
+        var context = state.context[state.context.length - 1];
+        if (ch === '"') {
+            state.inQuote = true;
+            state.quoteIdx = i;
+            continue;
+        }
+        else if (ch === ",") {
+            // comma is valid in context of array and object
+            if (state.tokens[state.tokens.length - 1] === 'v'
+                && (state.tokens[state.tokens.length - 2] === ':'
+                    || state.tokens[state.tokens.length - 2] === ','
+                    || state.tokens[state.tokens.length - 2] === '['
+                )) {
+                state.tokens.push(',');
+                continue;
+            }
+        }
+        else if (ch === ":") {
+            if (state.tokens[state.tokens.length - 1] === 'v' && context === 'o') {
+                state.tokens.push(':');
+                continue;
+            }
+        }
+
+        // ok as value
+        else if (ch === "{") {
+            if (state.tokens[state.tokens.length - 1] === ':' // expecting value --> ok
+                || (state.tokens[state.tokens.length - 1] === ',' // if previous is comma make sure it is not expecting key
+                    && (state.tokens[state.tokens.length - 3] === ',' || state.tokens[state.tokens.length - 3] === '['))
+                || state.tokens[state.tokens.length - 1] === '['
+            ) {
+                state.tokens.push('{');
+                state.context.push('o');
+                continue;
+            }
+        }
+
+        else if (ch === "[") {
+            if (state.tokens[state.tokens.length - 1] === ':' // expecting value --> ok
+                || (state.tokens[state.tokens.length - 1] === ',' // if previous is comma make sure it is not expecting key
+                    && (state.tokens[state.tokens.length - 3] === ',' || state.tokens[state.tokens.length - 3] === '['))
+                || state.tokens[state.tokens.length - 1] === '['
+            ) {
+                state.tokens.push('[');
+                state.context.push('a');
+                continue;
+            }
+        }
+        else if (ch === "}") {
+            if (state.tokens[state.tokens.length - 1] === '{'
+                || (state.tokens[state.tokens.length - 1] === 'v' && state.tokens[state.tokens.length - 2] === ':')) {
+                popUntil(state.tokens, '{');
+                state.context.pop();
+                if (state.tokens.length === 0) {
+                    jsons.push([state.jsonIdx, i]);
+                    state.jsonIdx = -1
+                }
+                else {
+                    state.tokens.push('v');
+                }
+                continue;
+            }
+        }
+
+        else if (ch === "]") {
+            if (state.tokens[state.tokens.length - 1] === '['
+                || (state.tokens[state.tokens.length - 1] === 'v'
+                    && (state.tokens[state.tokens.length - 2] === ',' || state.tokens[state.tokens.length - 2] === '['))) {
+                popUntil(state.tokens, '[');
+                state.context.pop();
+                if (state.tokens.length === 0) {
+                    jsons.push([state.jsonIdx, i]);
+                    state.jsonIdx = -1;
+                }
+                else {
+                    state.tokens.push('v');
+                }
+                continue;
+            }
+        }
+
+        else if (ch === "n") {
+            //null
+            if (str[i + 1] === "u" && str[i + 2] === "l" && str[i + 3] === "l") {
+                state.tokens.push('v');
+                i += 3;
+                continue;
+            }
+        }
+        else if (ch === "t") {
+            //true
+            if (str[i + 1] === "r" && str[i + 2] === "u" && str[i + 3] === "e") {
+                state.tokens.push('v');
+                i += 3;
+                continue;
+            }
+        }
+        else if (ch === "f") {
+            //false
+            if (str[i + 1] === "a" && str[i + 2] === "l" && str[i + 3] === "s" && str[i + 4] === "e") {
+                state.tokens.push('v');
+                i += 4;
+                continue;
+            }
+        }
+        else if ((ch === "-" || numbers.has(ch))
+            && (state.tokens[state.tokens.length - 1] === ':' // expecting value --> ok
+                || (state.tokens[state.tokens.length - 1] === ',' // if previous is comma make sure it is not expecting key
+                    && (state.tokens[state.tokens.length - 3] === ',' || state.tokens[state.tokens.length - 3] === '[')
+                ))) {
+            var expectDacimal = false;
+            var expectNumber = ch === '-';
+            var seenDot = false;
+
+            for (var j = i + 1; j < str.length; j++) {
+                if (numbers.has(str[j])) {
+                    i = j;
+                    if (expectNumber) {
+                        expectNumber = false;
+                    }
+                    else if (expectDacimal) {
+                        expectDacimal = false;
+                    }
+                    continue;
+                }
+                else if (str[j] === '.') {
+                    i = j
+                    expectDacimal = true;
+                    if (seenDot) {
+                        break;
+                    }
+                    seendot = true;
+                    continue;
+                }
+                break;
+            }
+            if (!expectNumber && !expectDacimal) {
+                state.tokens.push('v');
+                continue;
+            }
+        }
+
+        // invalid token at this point
+        // reset state and move on
+        if (state.inQuote) {
+            i = state.quoteIdx;
+        }
+        state = getInitialState();
+    }
+    return jsons;
+}
+
+function computeStartEnd(html, prefix, text) {
+    var start = -1;
+    var tag = false;
+    var whitespace = new Set([' ', '\t', '\r', '\n']);
+
+    var current_idx = 0;
+    var runner = 0;
     if (prefix) {
-        var div = document.createElement("div");
-        div.innerText = prefix;
-        node.appendChild(div);
+        for (; current_idx < html.length; current_idx++) {
+            var ch = html[current_idx];
+
+            if (whitespace.has(ch)) {
+                continue;
+            }
+
+            if (ch === ">") {
+                tag = false;
+                continue;
+            }
+
+            if (tag) {
+                continue;
+            }
+            if (ch === "<") {
+                tag = true;
+                continue;
+            }
+
+            if (ch === prefix[runner]) {
+                while (runner < prefix.length) {
+                    runner++;
+                    if (!whitespace.has(prefix[runner])) {
+                        break;
+                    }
+                }
+                if (runner === prefix.length) {
+                    current_idx++;
+                    break;
+                }
+            }
+            else {
+                console.log("doesn't match");
+            }
+        }
     }
-    node.appendChild(pre);
-    if (suffix) {
-        var div = document.createElement("div");
-        div.innerText = suffix;
-        node.appendChild(div);
+
+    runner = 0
+    tag = false;
+    for (; current_idx < html.length; current_idx++) {
+        var ch = html[current_idx];
+
+        if (ch === ">") {
+            tag = false;
+            continue;
+        }
+
+        if (tag) {
+            continue;
+        }
+        if (ch === "<") {
+            tag = true;
+            continue;
+        }
+        if (ch === text[runner]) {
+            if (start === -1) {
+                start = current_idx;
+            }
+            runner++;
+            if (runner === text.length) {
+                break;
+            }
+        }
     }
+
+    return [start, current_idx];
+}
+
+function escapeHtml(text) {
+    return text
+        .replace(/&/g, "&amp;")
+        .replace(/</g, "&lt;")
+        .replace(/>/g, "&gt;")
+        .replace(/"/g, "&quot;")
+        .replace(/'/g, "&#039;");
+}
+
+function wrapWithPreCode(node, prefix, formattedText, text) {
+    var html = node.innerHTML;
+    var [start, end] = computeStartEnd(html, prefix, text);
+    if (start === -1) {
+        return;
+    }
+    prefix = html.substr(0, start);
+    var suffix = html.substr(end + 1);
+
+    node.innerHTML = `${prefix}<div><pre><code>${escapeHtml(formattedText)}</pre></code></div>${suffix}`;
 }
 
 function replaceAsIs(node, prefix, text, suffix) {
-
     var result = [];
     if (prefix) {
         result.push(prefix);
@@ -58,7 +342,12 @@ function replaceAsIs(node, prefix, text, suffix) {
 
 function replace(node, text, unEscapeMode) {
 
-    var tmp = JSON.parse(text);
+    try {
+        var tmp = JSON.parse(text);
+    }
+    catch (err) {
+        return err;
+    }
 
     if (unEscapeMode) {
         if (Array.isArray(tmp)) {
@@ -70,6 +359,7 @@ function replace(node, text, unEscapeMode) {
     }
 
     var formattedText = JSON.stringify(tmp, null, 3);
+
     tmp = undefined;
     var nodeType = node.nodeName.toUpperCase();
 
@@ -78,13 +368,13 @@ function replace(node, text, unEscapeMode) {
     var end_idx = start_idx + text.length;
 
     var prefix = original.substr(0, start_idx);
-    var suffix = original.substr(end_idx);
 
     if (nodeType === "TEXTAREA") {
+        var suffix = original.substr(end_idx);
         replaceAsIs(node, prefix, formattedText, suffix);
     }
     else {
-        wrapWithPreCode(node, prefix, formattedText, suffix);
+        wrapWithPreCode(node, prefix, formattedText, text);
     }
 }
 
@@ -164,6 +454,12 @@ function unEscapeObj(obj) {
     }
 }
 
+function flash(node) {
+    addCSS();
+    node.classList.add("jippy-cant-parse");
+    setTimeout(() => { node.classList.remove("jippy-cant-parse") }, 510);
+}
+
 chrome.runtime.onMessage.addListener(
     function (message) {
         try {
@@ -173,7 +469,10 @@ chrome.runtime.onMessage.addListener(
 
             var selection = window.getSelection();
             var selectedText = selection.toString().trim();
-            var node = selection.focusNode; // focusNode == last node the selection fall on; SEE: https://developer.mozilla.org/en-US/docs/Web/API/Selection#Properties
+            var node = selection.focusNode || selection.anchorNode; // anchorNode == first, focusNode == last node the selection fall on; SEE: https://developer.mozilla.org/en-US/docs/Web/API/Selection#Properties
+            if (!node) {
+                return;
+            }
             if (node.nodeType === 3) { // TEXT NODE SEE: https://developer.mozilla.org/en-US/docs/Web/API/Node/nodeType#Constants
                 node = node.parentNode
             }
@@ -181,18 +480,39 @@ chrome.runtime.onMessage.addListener(
             if (!node.innerText.includes(selectedText)) {
                 node = findNodeByText(node, selectedText);
             }
-            if (
-                !(selectedText.startsWith("{") && selectedText.endsWith("}"))
-                && !(selectedText.startsWith("[") && selectedText.endsWith("]"))
-            ) {
-                selectedText = guess(node, selection);
-            }
 
-            if (!selectedText) {
+            if (!node) {
                 return;
             }
 
-            replace(node, selectedText, message === "Jipy:fmtJSONString");
+            if ((selectedText.startsWith("{") && selectedText.endsWith("}"))
+                || (selectedText.startsWith("[") && selectedText.endsWith("]"))) {
+                err = replace(node, selectedText, message === "Jipy:fmtJSONString");
+                if (!err) {
+                    return;
+                }
+            }
+            var longtext = node.innerText || node.value;
+            longtext = longtext.replace(/\\"/g, '"');
+            var candidates = extractJSONs(longtext);
+
+            if (candidates.length === 0) {
+                flash(node);
+                return;
+            }
+
+            var errorCount = 0;
+            for (var i = 0; i < candidates.length; i++) {
+                err = replace(node, longtext.substr(candidates[i][0], candidates[i][1] - candidates[i][0] + 1), message === "Jipy:fmtJSONString");
+                if (err) {
+                    errorCount++;
+                    notify(err);
+                }
+            }
+
+            if (errorCount === candidates.length) {
+                flash(node);
+            }
         }
         catch (err) {
             notify(err);
